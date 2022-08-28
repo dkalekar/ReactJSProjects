7791491643d2e7de19d80bc47dca9b50cc97cd0e
dkalekar <kalekar.dhanashree89@gmail.com>
2022-08-28 22:45:34 +0530

    Adding repo to Git

---
+++ src/services/CryptoAPI.js
@@ -0,0 +1,35 @@
+// In this function we are fetching the data from Rapid Api.
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "41616a33c9mshbca7cdc5ac1bad4p13ae12jsnbabb26a96ecc",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCrypto: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetials: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptoQuery,
+  useGetCryptoDetialsQuery,
+  useGetCryptoHistoryQuery,
+  useGetExchangesQuery,
+} = cryptoApi;
